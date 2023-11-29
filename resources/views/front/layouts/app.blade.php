c49245e320609b842d4fc9be271097718f3c973a
Md. Rahat Hossain <rhossainrahat@gmail.com>
2023-11-29 20:10:51 +0600

    change company name to AmarShop

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html class="no-js" lang="en_AU" />
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>LARAVEL ONLINE SHOP</title>
+    <title>AmarShop</title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
 
